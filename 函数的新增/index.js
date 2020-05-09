ed3b2457f956eb635d91d8f26484d0edc85b8eb8
jc <1392227049@qq.com>
2020-05-09 18:10:52 +0800

    '函数的新增'

---
+++ 函数的新增/index.js
@@ -0,0 +1,33 @@
+
+// 设置默认值
+
+    function defalutArg(name, name1, name) {
+        console.log(name)
+    }
+    defalutArg()
+
+    let sum = 10
+    function defalutArg1(n = sum + 1) {
+        console.log(n)
+    }
+    defalutArg1(sum) // 10
+
+    let x = 99;
+    function foo(p = x + 1) {
+        console.log(p);
+    }
+    foo() // 100
+
+    ' 对象结构 '
+    ' undefined特殊参数 将不会影响形参结构行为 '   
+    function foo1({ name = 1 } = {}) {
+        console.log(name)
+    }
+    // foo1( { name: undefined } )
+    
+
+    function n(a) {
+        const a = 2
+        console.log(a)
+    }
+    n()
